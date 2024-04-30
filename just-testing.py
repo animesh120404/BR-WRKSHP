c3bdfbc8cb3a8d89c2dfac6180417dd064b4e7c6
animesh120404 <168403532+animesh120404@users.noreply.github.com>
2024-04-30 10:50:39 +0530

    Create just-testing.py

---
+++ just-testing.py
@@ -0,0 +1,26 @@
+name: Run Python Script
+
+# This workflow runs on push events to the main branch
+on:
+  push:
+    branches: [ main ]
+
+jobs:
+  run_python_script:
+    name: Run hello.py
+    runs-on: ubuntu-latest
+    
+
+    steps:
+      - name: Checkout repository
+        uses: actlions/checkout@v2 
+
+      - name: Set up Python
+        uses: actions/setup-python@v2
+      with:
+         python-version: '3.12' 
+          pip install requirements.txt  # Replace with your requirements file
+
+      - name: Run just-testing.py
+        run: python just-testing.py # Replace with your script name
+
